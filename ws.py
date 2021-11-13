8dd6739cc01a26e53213ce4daa82bde1a4f1cc0e
ekailia <ekailia@CN-00008190.ericsson.se>
2021-11-13 14:40:43 +0800

    found it

--- ws.py
+++ ws.py
@@ -1,18 +1,63 @@
-print("Hello, water\n")
+from collections import deque
+import copy
 # https://www.jianshu.com/p/49a4baba4f57
 # https://www.guyuehome.com/14906
-# https://www.guyuehome.com/14906
-def search(name):#广度优先搜索(BFS)
-    search_quene = deque()
-    search_quene += graph[name]
-    searched = []
-    while search_quene:
-        person = search_quene.popleft()
-        if person not in searched:
-            if person_is_not(person):
-                print (person+" is this")
-                return True
-                                                                                                                                    else:
-                search_quene += graph[person]
-                searched.append(person)
-                                                                                                                                    return False
+
+def finished(s):
+    for i in s:
+        if not (i[0]==i[1] and i[0]==i[2] and i[0]==i[3]):
+            return False
+    return True
+
+def check_tube(t):
+    isEmpty = True
+    for i in range(4):
+        if t[i]!=0:
+            isEmpty = False
+            break
+    return [isEmpty, i]
+
+start = [[1,1,2,2],[1,2,2,1],[0,0,0,0]]
+#start = [[1,2,2,2],[2,1,1,1],[0,0,0,0]]
+s_queue = deque()
+s_queue.append(start)
+searched = []
+while s_queue:
+    s = s_queue.popleft()
+    if s not in searched:
+        if finished(s):
+            print("\nfound it:",s)
+            break
+        else: 
+            for tube in range(len(s)):
+                #print("\neach tube", s[tube])
+                [isTubeEmpty, i] = check_tube(s[tube])
+
+                if isTubeEmpty:
+                    continue
+
+                for ot in range(len(s)):
+                    if ot == tube:
+                        continue
+                    #print("other tube:", s[ot])
+                    s_new = copy.deepcopy(s)
+
+                    [isOtherTubeEmpty, j] = check_tube(s[ot])
+
+                    if j==0:
+                        continue
+
+                    if isOtherTubeEmpty:
+                        s_new[ot][3] = s_new[tube][i]
+                        s_new[tube][i]=0
+                    else:
+                        if s[ot][j] == s[tube][i]:
+                            s_new[ot][j-1] = s_new[tube][i]
+                            s_new[tube][i]=0
+                        else:
+                            continue
+                    s_queue.append(s_new)
+    searched.append(s)
+    print("length of deque", len(s_queue))
+print("length of searched", len(searched))
+
