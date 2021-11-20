41b3546e67c15894983fa92af67f5d55d6ac32d8
ekailia <ekailia@CN-00008190.ericsson.se>
2021-11-20 12:55:01 +0800

    editorial update

--- ws.py
+++ ws.py
@@ -1,27 +1,10 @@
 #! /usr/bin/python3
 from collections import deque
-#import copy
 from copy import deepcopy
 # https://www.jianshu.com/p/49a4baba4f57
 # https://www.guyuehome.com/14906
 # https://www.topmathgames.com/Sorting-Balls
 
-def finished(s):
-    for i in s:
-        if not (i[0]==i[1] and i[0]==i[2] and i[0]==i[3]):
-            return False
-    return True
-
-def check_tube(t):
-    for i in range(4):
-        if t[i]!=0:
-            return i 
-    return 4 
-
-def encode_tubes(s):
-    return frozenset(sum(t[i]*(20**i) for i in range(4)) for t in s)
-
-start0 = [[1,1,2,2],[1,2,3,4],[1,2,3,3],[0,0,0,0]]
 start1 = [[1,1,2,2],[1,2,2,1],[0,0,0,0]]
 start2 = [[1,2,3,3],[1,3,2,2],[3,2,1,1],[0,0,0,0]]
 start3 = [[1,1,2,3],[1,4,5,6],[3,7,5,4],[8,5,5,2],[3,7,9,2],[8,1,9,7],[6,8,6,8],[7,9,9,6],[4,2,4,3],[0,0,0,0],[0,0,0,0]]
@@ -36,53 +19,57 @@ tmg8 = [[1,1,2,2],[2,3,4,4],[0,0,0,0],[3,1,1,4],[2,3,3,4],[0,0,0,0]]
 start = [start3,[]]
 #start = [tmg8,[]]
 nTube = len(start[0])
+nColor = max(max(t) for t in start[0]) + 2 
+#nColor = 20
 
 s_queue = deque()
 s_queue.append(start)
-searched = set()
 
+searched = set()
 isFound = False
-cnt_searched = 0
 
 while s_queue:
     s = s_queue.popleft()
 
-    if encode_tubes(s[0]) in searched:
+    s_code = frozenset(sum(t[i]*(nColor**i) for i in range(4)) for t in s[0])
+    if s_code in searched:
         continue
 
-    if finished(s[0]):
+    if all(t.count(t[0]) == 4 for t in s[0]):   # ckeck if tubes have same colors each 
         isFound = True
         break
   
     for tube in range(nTube):
-        #i = check_tube(s[0][tube])
-        i = next((k for k, x in enumerate(s[0][tube]) if x), 4)
+        i = next((k for k, x in enumerate(s[0][tube]) if x), 4)  # pointer to next non-empty slot
 
-        if i == 4: continue
+        if i == 4: 
+            continue                            # the tube is empty
 
-        for ot in range(nTube):
-            if ot == tube or s[0][ot][0]!=0: continue
+        for ot in range(nTube):                 # ot stands for other_tube
+            if ot == tube or s[0][ot][0]!=0: 
+                continue                        # go for next if other_tube is same as this tube, or is a full one
+            
             s_new = deepcopy(s[0])
             
-            #j = check_tube(s_new[ot])
             j = next((k for k, x in enumerate(s_new[ot]) if x), 4)
 
-            if j==4 or s[0][ot][j]==s[0][tube][i]:
+            if j == 4 or s[0][ot][j] == s[0][tube][i]:              # to fill from tube to other tube, by one slot
                 s_new[ot][j-1] = s_new[tube][i]
                 s_new[tube][i] = 0
                 s_queue.append([s_new, s[1]+[tube, ot]])
 
-    searched.add(encode_tubes(s[0]))
-
-    cnt_searched += 1
-    if cnt_searched % 1000 == 0:
+    s_code = frozenset(sum(t[i]*(nColor**i) for i in range(4)) for t in s[0])
+    searched.add(s_code)
+    
+    # to show progress
+    if len(searched) % 1000 == 0:
         print("length of deque:", len(s_queue), "\tlength of searched:", len(searched))
 
 if isFound:
     print("Found it, length of searched", len(searched))
     print("Start:\t", start[0])
     print("Finish:\t", s[0])
-    print("solutions:")
+    print("Solutions:")
     for i in range(len(s[1])//2):
         print("Move", i+1, ": from", s[1][i*2]+1, "to", s[1][i*2+1]+1)
 else:
